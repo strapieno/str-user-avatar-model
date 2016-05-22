65d696adf2390a13ab8a3d85fd706407b8083191
visa4 <antonino.visalli@gmail.com>
2016-05-22 17:40:49 +0200

    Add User avatar entity

---
+++ src/Entity/UserAvatarEntity.php
@@ -0,0 +1,12 @@
+<?php
+namespace Strapieno\UserAvatar\Model\Entity;
+
+use ImgMan\Apigility\Entity\ImageEntity;
+
+/**
+ * Class UserAvatarEntity
+ */
+class UserAvatarEntity extends ImageEntity
+{
+
+}
